76496aab1a5f063e8fc49307b2a01e546b498538
canuham <44009021+canuham@users.noreply.github.com>
2018-10-15 14:30:29 -0500

    master
    Taller terminado

---
+++ TallerAPI.Xamarin/TallerAPI.Xamarin/Views/ListViewPage1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TallerAPI.Xamarin.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerAPI.Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage1 : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public ListViewPage1()
+        {
+            InitializeComponent();
+            CargarPublicaciones();
+        }
+        private async Task CargarPublicaciones()
+        {
+            HttpClient cliente = new HttpClient();
+
+            //Se especifica el al cual se le realizaran los request
+            cliente.BaseAddress = new Uri("http://172.16.21.9");
+            var request =  await cliente.GetAsync("/Publicacion/api/Publicacion");
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJSON = await request.Content.ReadAsStringAsync();
+                var respuesta = JsonConvert.DeserializeObject<List<Publicacion>>(responseJSON);
+                MyListView.ItemsSource = respuesta;
+            }
+        }
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Elemento seleccionado", ":)", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;        
+        }
+    }
+}
---
+++ TallerAPI.apis/Controllers/PublicacionController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TallerAPI.apis.Models;
+
+namespace TallerAPI.apis.Controllers
+{
+    public class PublicacionController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Publicacion> Get()
+        {
+            using(var context = new PublicacionContext())
+            {
+                return context.Publicaciones.ToList();
+            }
+        }
+
+        [HttpGet]
+        public Publicacion Get(int id)
+        {
+            using (var context = new PublicacionContext())
+            {
+                return context.Publicaciones.FirstOrDefault(x=> x.Id == id);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(Publicacion publicacion)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (var context = new PublicacionContext())
+            {
+                context.Publicaciones.Add(publicacion);
+                context.SaveChanges();
+                return Ok(publicacion);
+            }
+        }
+
+        [HttpPut]
+        public Publicacion Put(Publicacion publicacion)
+        {
+            using (var context = new PublicacionContext())
+            {
+                var publicacionAct = context.Publicaciones.FirstOrDefault(x => x.Id == publicacion.Id);
+                publicacionAct.Usuario = publicacion.Usuario;
+                publicacionAct.Descripcion = publicacion.Descripcion;
+                publicacionAct.FechaPublicacion = publicacion.FechaPublicacion;
+                publicacionAct.MeGusta = publicacion.MeGusta;
+                publicacionAct.MeDisgusta = publicacion.MeDisgusta;
+                publicacionAct.VecesCompartido = publicacion.VecesCompartido;
+                publicacionAct.EsPrivada = publicacion.EsPrivada;
+                
+                context.SaveChanges();
+                return publicacion;
+            }
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            using (var context = new PublicacionContext())
+            {
+                var publicacionDel = context.Publicaciones.FirstOrDefault(x => x.Id == id);
+                context.Publicaciones.Remove(publicacionDel);
+                context.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
+
